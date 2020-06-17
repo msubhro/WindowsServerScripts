bb7ea1d0c5441d26fa1ee23e2fb868cefb0a52cf
Subhro Majumder <48013811+msubhro@users.noreply.github.com>
2020-06-17 13:27:58 +0530

    Add files via upload

---
+++ CaptureRegistryValue/GetRegValue V 1.0.ps1
@@ -0,0 +1,30 @@
+﻿
+Write-Host "  "
+
+$inputpath= Read-Host "Please enter the location of the input file. This should be a TXT file containing list of systems."
+
+Write-Host "  "
+
+$outputpath= Read-Host "Please enter the location of the output file. This should be a TXT file where all details would be extracted."
+
+Write-Host "  "
+
+$Serverlist = get-content -Path $inputpath
+
+$output= foreach ($Server in $ServerList)
+
+{
+
+Write-Host "  "
+
+Write-Output "------------------------------------------------------"
+
+Write-Output "Server Name : $Server"
+
+Write-Output "------------------------------------------------------"
+
+Invoke-Command -ComputerName $Server -ScriptBlock { Get-ItemProperty -Path "hklm:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"}
+
+}
+
+$output | out-file -FilePath $outputpath -Append
